ad40af4859246da536adbc9c96b9c032e6f826c3
luintas <nelissen.flaharty3@gmail.com>
2019-01-23 16:29:50 +0100

    firstcommit

---
+++ Position.java
@@ -0,0 +1,33 @@
+public class Position{
+    private int col,lig;
+
+    public Position(int col,int lig){
+        this.col=col;
+        this.lig=lig;
+    }
+    /**
+     * @return the col
+     */
+    public int getCol() {
+        return col;
+    }
+    /**
+     * @return the lig
+     */
+    public int getLig() {
+        return lig;
+    }
+    @Override
+    public boolean equals(Object obj) {
+        if(this==obj)
+            return true;
+        if(this==null)
+            return false;
+        if(this.getClass()==obj.getClass()){
+            Position p= (Position) obj;
+            if((this.col==p.col)&&(this.lig==p.lig))
+                return true;
+        }
+        return false;
+    }
+}
